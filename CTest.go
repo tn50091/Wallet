a7dbfd98b72f4782b33da147be526144c882408e
tn55043 <parisa.th@tnis.com>
2017-10-22 23:41:22 +0700

    Test

--- CTest.go
+++ CTest.go
@@ -1,20 +1,38 @@
 package main
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+
+	"log"
+	"fmt"
+	"time"
+	"flag"
+	"regexp"
 )
 
-type rqCreate struct {
-	WalletID		string	`json:"walletid"`
-	CitizenID		string	`json:"citizenid"`
-	FullName		string	`json:"fullname"`
-	OpenDateTime	string	`json:"opendatetime"`
-	LedgerBal		string	`json:"ledgerbal"`
+var addr = flag.String("addr", "127.0.0.1:8082", "Rest Service")
+
+//English Character only + “,” + “-“ + “.” + “ “
+var IsLetter = regexp.MustCompile(`^[a-zA-Z ,.-]+$`).MatchString
+
+type Ewallet struct {
+	Fullname		string	`json:"fullname"`
+	Citizenid		string	`json:"citizenid"`
+}
+
+func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "{message: %q}", message)
+}
+
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	w.Write(json)
 }
 
 func main(){
@@ -27,12 +45,9 @@ func main(){
 	session.SetMode(mgo.Monotonic, true)
 	ensureIndex(session)
 
-	collection := session.DB("eWallet").C("customer")
-	err = collection.Insert(bson.M{"name": "TestB","citizenID": "2222222222222"})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Insert data success.")
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/ewallets", createEwallet(session))
+	http.ListenAndServe(*addr, nil)
 }
 
 func ensureIndex(s *mgo.Session) {
@@ -42,7 +57,7 @@ func ensureIndex(s *mgo.Session) {
 	c := session.DB("eWallet").C("account")
 
 	index := mgo.Index{
-		Key:        []string{"walletid"},
+		Key:        []string{"wallet_id"},
 		Unique:     true,
 		DropDups:   true,
 		Background: true,
@@ -53,3 +68,81 @@ func ensureIndex(s *mgo.Session) {
 		panic(err)
 	}
 }
+
+func checkCitizenID(cid string) bool {
+	error := false
+	if len(cid) != 13 {
+		error = true
+	}
+	return error
+}
+
+func findCitizenID(s *mgo.Session, cid string) bool {
+	error := false
+
+	session := s.Copy()
+	defer session.Close()
+
+	return error
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Welcome to E-Wallet")
+}
+
+func createEwallet(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		var ewallet Ewallet
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&ewallet)
+		if err != nil {
+			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+			return
+		}
+
+		fullname := ewallet.Fullname
+		if IsLetter(fullname) != true {
+			ErrorWithJSON(w, "Invalid Full Name", http.StatusBadRequest)
+			return
+		}
+
+		citizen := ewallet.Citizenid
+		if checkCitizenID(citizen) != false {
+			ErrorWithJSON(w, "Invalid Citizen ID", http.StatusBadRequest)
+			return
+		}
+
+		c := session.DB("eWallet").C("account")
+
+		err = c.Insert(bson.M{"wallet_id": "00003","citizen_id": citizen,"full_name": fullname,"open_datetime": time.Now(),"ledger_balance": "0.00"})
+		if err != nil {
+			if mgo.IsDup(err) {
+				ErrorWithJSON(w, "E-Wallet already exists", http.StatusBadRequest)
+				return
+			}
+
+			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+			log.Println("Failed insert E-Wallet: ", err)
+			return
+		}
+
+		/*
+		var resultTask Task
+		resultTask.ID = task.ID
+		resultTask.Title = task.Title
+		resultTask.Done = task.Done
+
+		if err := json.NewEncoder(w).Encode(resultTask); err != nil {
+			log.Println(err)
+			http.Error(w, "oops", http.StatusInternalServerError)
+		}
+		*/
+		//w.Header().Set("Content-Type", "application/json")
+		//w.Header().Set("Location", r.URL.Path+"/"+book.ISBN)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
